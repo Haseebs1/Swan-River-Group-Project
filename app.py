ecaa95c2dddbaea23fb00502706af5f1311f6be0
Haseebs1 <haseebs1@outlook.com>
2025-02-11 17:25:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import 
 
 app = Flask(_name_)
 
 @app.route("/")
-def index();
-return render_template("index.html")
+def Hello();
+return "Hello World!"
 
-if _name_ = "_main_":
-app.run(host= "0.0.0.0")
