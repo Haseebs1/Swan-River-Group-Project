8d47e08099c5fcd41e2f35b01ae621ce019fe962
Haseebs1 <haseebs1@outlook.com>
2025-02-11 21:17:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -149,6 +149,6 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(debug=True, host="0.0.0.0", port=8000)
 
 
