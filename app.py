0c9756c1b0099867da8798c2f81a36804f6df0fe
Haseebs1 <haseebs1@outlook.com>
2025-02-11 21:03:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,11 @@ CLIENT_ID = "920e0dcb-6b9b-4ae1-8038-8b57c277dec3"  # Replace with your Azure AD
 CLIENT_SECRET = "Kxb8Q~PV~cRSKJYeVjLi9YFWoFFjhsNWT4P.Ydcb"  # Replace with your Azure AD app's Client Secret
 TENANT_ID = "170bbabd-a2f0-4c90-ad4b-0e8f0f0c4259"  # Replace with your Azure AD Tenant ID
 AUTHORITY = f"https://login.microsoftonline.com/170bbabd-a2f0-4c90-ad4b-0e8f0f0c4259"
-REDIRECT_PATH = "http://swanriver-hpchbdasddbqfxd4.centralus-01.azurewebsites.net/login/authorized"  # Must match the redirect URI in Azure AD
+REDIRECT_PATH = "https://swanriver-hpchbdasddbqfxd4.centralus-01.azurewebsites.net/login/authorized"  # Must match the redirect URI in Azure AD
 SCOPE = ["User.Read"]  # Permissions to request
 
+# OAuth2 session
+oauth = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE)
 # Flask-Login setup
 login_manager = LoginManager()
 login_manager.init_app(app)
