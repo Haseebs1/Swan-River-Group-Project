dec9155af3bb5b8a255a882f1526457c6daf2408
Haseebs1 <haseebs1@outlook.com>
2025-02-11 18:22:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
-def Hello();
-return "Hello World!"
+def hello():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
 
