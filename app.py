d69eea2b899e61cf01f862700fdedb88641fccb8
Haseebs1 <haseebs1@outlook.com>
2025-02-20 19:08:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ class User(db.Model):
 
 # Check if user is logged in
 def login_required(f):
+    @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'user' not in session:
             return redirect(url_for('login'))
